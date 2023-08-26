61a036ee271457eb63f82eea28f4b02be6564c0e
Ghali Joudary <ghalijoudary03@gmail.com>
2023-08-26 18:22:34 +0100

    added a basic navbar

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavbarWithMegaMenu from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarWithMegaMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const colors = {
+  blue: "bg-blue-50 text-blue-500",
+  orange: "bg-orange-50 text-orange-500",
+  green: "bg-green-50 text-green-500",
+  "blue-gray": "bg-blue-gray-50 text-blue-gray-500",
+  purple: "bg-purple-50 text-purple-500",
+  teal: "bg-teal-50 text-teal-500",
+  cyan: "bg-cyan-50 text-cyan-500",
+  pink: "bg-pink-50 text-pink-500",
+};
+
+export default function NavbarWithMegaMenu() {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  return (
+    <Navbar className="bg-gray-400 mx-auto max-w-screen-2xl px-4 py-2">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="mr-4 cursor-pointer py-1.5 lg:ml-2"
+        >
+          Logo
+        </Typography>
+        <div className="hidden lg:block"></div>
+        <div className="hidden gap-2 lg:flex">
+          <Button variant="text" size="sm" color="blue-gray">
+            Sign In
+          </Button>
+          <Button variant="gradient" size="sm">
+            Sign Up
+          </Button>
+        </div>
+      </div>
+    </Navbar>
+  );
+}
